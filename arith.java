54799c929d2d01978df875071d84514a5e217b16
Shreeraj-drwx <shreeraj_u@my.ofs.edu.sg>
2023-05-10 18:35:22 +0800

    h
    h

---
+++ arith.java
@@ -0,0 +1,37 @@
+public class arith
+{
+    public static void main(String [ ] args)
+
+    {
+        int start = IBIO.inputInt("Enter your first value ");
+        int step = IBIO.inputInt("Enter the number of steps ");
+        int increment = IBIO.inputInt("Enter the increment per step ");
+
+
+        while(step > 0);
+        System.out.println(start);
+        step--;
+        start = start + increment;
+
+
+
+
+        //for( step > 0 ; step < 0 ;  step--);
+        //	 System.out.println(start);
+        //	 start = start + increment;
+        //	 step = step - 1;
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
